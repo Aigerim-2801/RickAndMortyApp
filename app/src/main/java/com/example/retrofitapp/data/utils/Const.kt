63507020c100607efc31cec852bac5698363a67a
Aigerim-2801 <aygerim.berdimurat.03@inbox.ru>
2023-06-20 20:23:23 +0600

    const

--- app/src/main/java/com/example/retrofitapp/data/utils/Const.kt
+++ app/src/main/java/com/example/retrofitapp/data/utils/Const.kt
@@ -2,4 +2,9 @@ package com.example.retrofitapp.data.utils
 
 object Const {
     const val BASE_URL = "https://rickandmortyapi.com/api/"
+    const val LOCATION_ID = "location_id"
+    const val EPISODE_ID = "episode_id"
+    const val CHARACTER_ID = "character_id"
+    const val FILTER_KEY = "FilterCharacters"
+    const val THEME_PREFERENCE_KEY = "theme_preference"
 }
